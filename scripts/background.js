27f294b204fd55bb35b34991e9f85eec34695fc8
Alex <alexander@kwaschny.net>
2018-09-01 17:34:14 +0200

    messages now forwarded to all tabs matching twitch.tv

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,11 @@
 ﻿// jshint esversion: 6
 
+const twitchUrl = 'https://www.twitch.tv/';
+
 // enable icon on twitch.tv tabs only
 function checkForSupportedURL(tabID, changeInfo, tab) {
 
-	if ((typeof tab.url === 'string') && (tab.url.indexOf('https://www.twitch.tv') === 0)) {
+	if ((typeof tab.url === 'string') && (tab.url.indexOf(twitchUrl) === 0)) {
 
 		chrome.pageAction.show(tabID);
 	}
@@ -11,29 +13,33 @@ function checkForSupportedURL(tabID, changeInfo, tab) {
 chrome.tabs.onUpdated.addListener(checkForSupportedURL);
 
 // forward all messages to content script
+function forwardMessageToTabs(request, tabs, isGlobal) {
+
+	let relevantTabs = [];
+
+	let tabsLength = tabs.length;
+	for (let i = 0; i < tabsLength; i++) {
+
+		if (typeof tabs[i].url === 'string') {
+
+			relevantTabs.push(tabs[i]);
+		}
+	}
+
+	tabsLength = relevantTabs.length;
+	for (let i = 0; i < tabsLength; i++) {
+
+		chrome.tabs.sendMessage(relevantTabs[i].id, request);
+
+		if (isGlobal === true) { break; }
+	}
+}
 chrome.runtime.onMessage.addListener(function(request) {
 
 	const isGlobal = (request && (typeof request.blacklistedItems === 'object'));
 
-	chrome.tabs.getAllInWindow(null, function(tabs) {
+	chrome.tabs.query({ url: (twitchUrl + '*') }, function(tabs) {
 
-		let relevantTabs = [];
-
-		let tabsLength = tabs.length;
-		for (let i = 0; i < tabsLength; i++) {
-
-			if (typeof tabs[i].url === 'string') {
-
-				relevantTabs.push(tabs[i]);
-			}
-		}
-
-		tabsLength = relevantTabs.length;
-		for (let i = 0; i < tabsLength; i++) {
-
-			chrome.tabs.sendMessage(relevantTabs[i].id, request);
-
-			if (isGlobal === true) { break; }
-		}
+		forwardMessageToTabs(request, tabs, isGlobal);
 	});
 });
