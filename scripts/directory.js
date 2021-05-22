1a9d431d62b17205dd35faf11ec46772134b85a5
Alexander Kwaschny <alexander@kwaschny.net>
2021-05-22 19:15:56 +0200

    Revert "only fetch recommended channels on "Following" pages when hideFollowing is false"
    This reverts commit f6e256e1ea1ec08baed6f2a1d4202452b3a0859d.

--- scripts/directory.js
+++ scripts/directory.js
@@ -387,13 +387,7 @@
 				return;
 			}
 
-			let mode = 'unprocessed';
-			if (hideFollowing === false) {
-
-				mode = 'recommended';
-			}
-
-			const nodes = getDirectoryItemNodes(mode);
+			const nodes 		= getDirectoryItemNodes('unprocessed');
 			const nodesLength 	= nodes.length;
 
 			// when there are unprocessed items in the directory, assume that the user scrolled down
