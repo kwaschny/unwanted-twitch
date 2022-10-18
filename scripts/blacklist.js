kwaschny/unwanted-twitch

commit 25def56a98cd64b515e8bd134319c3f0a1f57bf7

normalize case of all blocked terms

scripts/blacklist.js
@@ -9,6 +9,8 @@
 
 function createItemRow(key) {
 
+	key = normalizeCase(key);
+
 	let row = document.createElement('tr');
 	row.className = 'item';
 
@@ -60,7 +62,7 @@ function addItems(table, items) {
 	// sort items (case insensitive)
 	sortedKeys = sortedKeys.sort(function (a, b) {
 
-		return a.toLowerCase().localeCompare( b.toLowerCase() );
+		return normalizeCase(a).localeCompare( normalizeCase(b) );
 	});
 
 	let fragment = document.createDocumentFragment();
@@ -95,8 +97,8 @@ function clearItems(table) {
 
 function itemExists(table, key) {
 
-	key = key.toLowerCase();
-	const presentKeys = gatherKeysArray(table, true);
+	key = normalizeCase(key);
+	const presentKeys = gatherKeysArray(table);
 
 	return (presentKeys.indexOf(key) >= 0);
 }
@@ -130,13 +132,13 @@ function onAddItem(row) {
 
 				if (isCS) {
 
-					// preserve case and keep flag
+					// preserve input and keep flag
 					itemToAdd = ('/' + body + '/I');
 
 				} else {
 
-					// store manually added keys as all-lowercase
-					itemToAdd = ('/' + body + '/').toLowerCase();
+					// normalize input and discard flag
+					itemToAdd = ('/' + normalizeCase(body) + '/');
 				}
 
 				if (isCI) {
@@ -156,14 +158,14 @@ function onAddItem(row) {
 			itemToAdd = itemToAdd.replace(/^"/, '\'');
 			itemToAdd = itemToAdd.replace(/"$/, '\'');
 
-			// store manually added keys as all-lowercase
-			itemToAdd = itemToAdd.toLowerCase();
+			// store manually added key with normalized case
+			itemToAdd = normalizeCase(itemToAdd);
 		}
 
 	} else {
 
-		// store manually added keys as all-lowercase
-		itemToAdd = itemToAdd.toLowerCase();
+		// store manually added key with normalized case
+		itemToAdd = normalizeCase(itemToAdd);
 	}
 
 	if (itemToAdd.length > 0) {
@@ -227,7 +229,7 @@ function handleItemCount(table) {
 	return count;
 }
 
-function gatherKeysMap(table, allLowerCase) {
+function gatherKeysMap(table) {
 
 	let result = {};
 
@@ -236,19 +238,16 @@ function gatherKeysMap(table, allLowerCase) {
 
 	for (let i = 0; i < nodesLength; i++) {
 
-		let key = nodes[i].getAttribute('data-key');
-
-		if (allLowerCase === true) {
-
-			key = key.toLowerCase();
-		}
+		let key = normalizeCase(
+			nodes[i].getAttribute('data-key')
+		);
 
 		result[key] = 1;
 	}
 
 	return result;
 }
-function gatherKeysArray(table, allLowerCase) {
+function gatherKeysArray(table) {
 
 	let result = [];
 
@@ -257,12 +256,9 @@ function gatherKeysArray(table, allLowerCase) {
 
 	for (let i = 0; i < nodesLength; i++) {
 
-		let key = nodes[i].getAttribute('data-key');
-
-		if (allLowerCase === true) {
-
-			key = key.toLowerCase();
-		}
+		let key = normalizeCase(
+			nodes[i].getAttribute('data-key')
+		);
 
 		result.push(key);
 	}

scripts/directory.js
@@ -1188,9 +1188,9 @@
 
 			if (item.name.length > 0) {
 
-				// also check all-lowercase spelling
+				// check original and normalized case against blacklist
 				const itemName  = item.name;
-				const itemNameL = itemName.toLowerCase();
+				const itemNameL = normalizeCase(itemName);
 
 				const blacklistedByName = (
 					(storedBlacklistedItems[item.type][itemName]  !== undefined) ||
@@ -1207,9 +1207,9 @@
 
 			if (item.category.length > 0) {
 
-				// also check all-lowercase spelling
+				// check original and normalized case against blacklist
 				const categoryName  = item.category;
-				const categoryNameL = categoryName.toLowerCase();
+				const categoryNameL = normalizeCase(categoryName);
 
 				const blacklistedByCategory = (
 					(storedBlacklistedItems['categories'][categoryName]  !== undefined) ||
@@ -1233,9 +1233,9 @@
 
 				if (tag.name.length === 0) { continue; }
 
-				// also check all-lowercase spelling
+				// check original and normalized case against blacklist
 				const tagName  = tag.name;
-				const tagNameL = tagName.toLowerCase();
+				const tagNameL = normalizeCase(tagName);
 
 				if (
 					(storedBlacklistedItems['tags'][tagName]  !== undefined) ||
@@ -1256,7 +1256,7 @@
 			if ((item.title.length > 0) && (storedBlacklistedItems['titles'].length > 0)) {
 
 				const itemTitle  = item.title;
-				const itemTitleL = item.title.toLowerCase();
+				const itemTitleL = normalizeCase(item.title);
 
 				const titles       = storedBlacklistedItems['titles'];
 				const titlesLength = titles.length;
@@ -1264,7 +1264,7 @@
 				for (let i = 0; i < titlesLength; i++) {
 
 					const title  = titles[i];
-					const titleL = title.toLowerCase();
+					const titleL = normalizeCase(title);
 
 					if (title.length === 0) { continue; }
 
@@ -2261,7 +2261,9 @@
 
 			storedBlacklistedItems[item.type] = {};
 		}
-		storedBlacklistedItems[item.type][item.name] = 1;
+
+		const nameL = normalizeCase(item.name);
+		storedBlacklistedItems[item.type][nameL] = 1;
 
 		// update storage
 		putBlacklistedItems(storedBlacklistedItems);
@@ -2295,7 +2297,9 @@
 
 			storedBlacklistedItems['tags'] = {};
 		}
-		storedBlacklistedItems['tags'][tag.name] = 1;
+
+		const nameL = normalizeCase(tag.name);
+		storedBlacklistedItems['tags'][nameL] = 1;
 
 		// update storage
 		putBlacklistedItems(storedBlacklistedItems);

scripts/common.js
@@ -63,6 +63,19 @@ function isFirefox() {
 }
 
 /**
+ * Returns a normalized lowercase string without diacritics.
+ */
+function normalizeCase(term) {
+
+	return String(term)
+		.trim()
+		.normalize('NFKD')
+		.replace(/\p{Diacritic}/gu, '')
+		.toLowerCase()
+	;
+}
+
+/**
  * Wrapper to retrieve data from storage.
  */
 function storageGet(data, callback) {

commit 932c7ef90130af31becdd2d2b9a84d0ddb6bc1ee

do not normalize regular expressions
fixes regression caused by 25def56a98cd64b515e8bd134319c3f0a1f57bf7

scripts/blacklist.js
@@ -9,8 +9,6 @@
 
 function createItemRow(key) {
 
-	key = normalizeCase(key);
-
 	let row = document.createElement('tr');
 	row.className = 'item';
 
@@ -33,6 +31,11 @@ function createItemRow(key) {
 }
 
 function addItem(table, key) {
+
+	if (!tableAllowsRules(table)) {
+
+		key = normalizeCase(key);
+	}
 
 	// prevent adding the same key more than once
 	if (itemExists(table, key) === true) {
@@ -67,9 +70,15 @@ function addItems(table, items) {
 
 	let fragment = document.createDocumentFragment();
 
+	const allowsRules = tableAllowsRules(table);
+
 	for (let i = 0; i < sortedKeysLength; i++) {
 
 		let key = sortedKeys[i];
+		if (!allowsRules) {
+
+			key = normalizeCase(key);
+		}
 
 		fragment.appendChild(
 			createItemRow(key)
@@ -97,7 +106,6 @@ function clearItems(table) {
 
 function itemExists(table, key) {
 
-	key = normalizeCase(key);
 	const presentKeys = gatherKeysArray(table);
 
 	return (presentKeys.indexOf(key) >= 0);
@@ -113,8 +121,8 @@ function onAddItem(row) {
 	// remove consecutive whitespaces
 	itemToAdd = itemToAdd.replace(/[\s]{2,}/g, ' ');
 
-	// special matching support for titles
-	if (table.id === 'table_titles') {
+	// check for special matching support
+	if (tableAllowsRules(table)) {
 
 		const matches = itemToAdd.match(new RegExp('^/(.+)/([a-zA-Z]*)$'))
 
@@ -132,13 +140,13 @@ function onAddItem(row) {
 
 				if (isCS) {
 
-					// preserve input and keep flag
+					// keep flag
 					itemToAdd = ('/' + body + '/I');
 
 				} else {
 
-					// normalize input and discard flag
-					itemToAdd = ('/' + normalizeCase(body) + '/');
+					// discard flag
+					itemToAdd = ('/' + body + '/');
 				}
 
 				if (isCI) {
@@ -236,11 +244,15 @@ function gatherKeysMap(table) {
 	let nodes = table.querySelectorAll('[data-key]');
 	const nodesLength = nodes.length;
 
+	const allowsRules = tableAllowsRules(table);
+
 	for (let i = 0; i < nodesLength; i++) {
 
-		let key = normalizeCase(
-			nodes[i].getAttribute('data-key')
-		);
+		let key = nodes[i].getAttribute('data-key');;
+		if (!allowsRules) {
+
+			key = normalizeCase(key);
+		}
 
 		result[key] = 1;
 	}
@@ -254,11 +266,15 @@ function gatherKeysArray(table) {
 	let nodes = table.querySelectorAll('[data-key]');
 	const nodesLength = nodes.length;
 
+	const allowsRules = tableAllowsRules(table);
+
 	for (let i = 0; i < nodesLength; i++) {
 
-		let key = normalizeCase(
-			nodes[i].getAttribute('data-key')
-		);
+		let key = nodes[i].getAttribute('data-key');
+		if (!allowsRules) {
+
+			key = normalizeCase(key);
+		}
 
 		result.push(key);
 	}
@@ -444,6 +460,16 @@ function onTitlesExplained() {
 	alert(
 		chrome.i18n.getMessage('blacklist_TitlesExplainedText')
 	);
+}
+
+function tableAllowsRules(table) {
+
+	if (table && table.id) {
+
+		table = table.id;
+	}
+
+	return (table === 'table_titles');
 }
 
 function toggleLoadingScreen(show) {

scripts/directory.js
@@ -1198,7 +1198,6 @@
 				);
 
 				if (blacklistedByName) { return true; }
-
 			}
 
 		/* END: blacklisted by name */
@@ -1217,7 +1216,6 @@
 				);
 
 				if (blacklistedByCategory) { return true; }
-
 			}
 
 		/* END: blacklisted by category */
@@ -1294,6 +1292,7 @@
 									title.substring(1, (title.length -2))
 								);
 
+								// match against regular title (regular expression body is not normalized)
 								if (regex.test(itemTitle) === true) { return true; }
 
 							// case-insensitive
@@ -1303,7 +1302,8 @@
 									titleL.substring(1, (titleL.length -1)), 'i'
 								);
 
-								if (regex.test(itemTitleL) === true) { return true; }
+								// match against regular title (regular expression body is not normalized)
+								if (regex.test(itemTitle) === true) { return true; }
 							}
 
 						// loose match